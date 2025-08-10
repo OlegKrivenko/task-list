f452f898a91c4bfcb09c598b3e9fe02f368e5a65
OlegKrivenko <krivenko.olegus@gmail.com>
2025-08-10 23:12:23 +0300

    create css for checkbox in component Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,17 +1,18 @@
-import { MdClose } from 'react-icons/md';
+import { MdOutlineDelete } from 'react-icons/md';
 import css from './Task.module.css';
 
 export const Task = ({ task }) => {
   return (
     <div className={css.wrapper}>
-      <input
-        type="checkbox"
-        className={css.checkbox}
-        checked={task.completed}
-      />
+      <label className={css['custom-checkbox']}>
+        {/* <input type="checkbox" checked={task.completed} /> */}
+        <input type="checkbox" />
+        <span className={css.checkmark}></span>
+      </label>
+
       <p className={css.text}>{task.text}</p>
       <button className={css.btn}>
-        <MdClose size={24} />
+        <MdOutlineDelete size={26} />
       </button>
     </div>
   );
