d9fc9e8306347f619a60a36f46bba8b06f03a326
OlegKrivenko <krivenko.olegus@gmail.com>
2025-08-15 00:12:50 +0200

    create case reducer deleteTask

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -19,7 +19,11 @@ const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'tasks/addTask':
       return { ...state, tasks: [...state.tasks, action.payload] };
-
+    case 'tasks/deleteTask':
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== action.payload),
+      };
     default:
       return state;
   }
