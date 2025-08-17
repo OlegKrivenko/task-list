5d110dbca2384299ce2c58258f20b6f321d1d1f2
OlegKrivenko <krivenko.olegus@gmail.com>
2025-08-17 22:33:51 +0200

    use Redux Toolkit

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
 import { rootReducer } from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+export const store = configureStore({ reducer: rootReducer });
